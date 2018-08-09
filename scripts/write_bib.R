b71d7d3989286fa07e039ef929ad76677adc95b3
asocrai <asocrai@hotmail.com>
2018-08-09 15:45:09 +0200

    laTex for mathematical equations

--- scripts/write_bib.R
+++ scripts/write_bib.R
@@ -1,6 +1,10 @@
-# Add the DT and rorcid packages to the function so the packages.bib file has the references we need for the embedded data table ORCID integration processes executed in this lesson.
+# Add the DT and rorcid packages to the function so the packages.
+# bib file has the references we need for the embedded data table ORCID integration 
+# processes executed in this lesson.
+
+# DT : Dynamic Datatables
 
 library(tidyverse)
 library(knitr)
-write_bib(c("tidyverse", "rmarkdown", "knitr", "DT", "rorcid"),
-          file = "packages.bib")
+write_bib(c("tidyverse", "rmarkdown", "knitr", "DT", "rorcid", "DT", "rorcid"),
+          file = "raiAshok_DT2018/packages.bib")
